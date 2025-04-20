f666fd0b3e6841f44b45f0f3e4f828e3879b29bc
Alberto Alegre - Enti <alberto.alegre@enti.cat>
2025-04-20 11:31:12 +0200

    Add project files.

---
+++ TCPConnectionSampleClient/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TCPConnectionSampleClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
